889a10ce543028c73a19d979a0e0f438547ad718
Papotta <mazzanga1212@gmail.com>
2022-01-17 19:08:13 -0300

    errer en funcion crear

--- core/settings.py
+++ core/settings.py
@@ -1,18 +1,18 @@
 
 from pathlib import Path
-import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p*52&yybf0dfw*hyes^9&8p$)^pfa^_!x(&b)f#iuch2g$r$yb'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',
+    'blog',
 ]
 
 MIDDLEWARE = [
